341fb5c2eae4962ff149d107bd81d08845a24b43
DebXD <debiprasadxd@protonmail.com>
2023-06-26 18:34:13 +0530

    made the player a separate component for easy use on other pages, convert seconds to minute and seconds digital clock format

--- src/app/albums/[albumName]/page.tsx
+++ src/app/albums/[albumName]/page.tsx
@@ -1,88 +1,24 @@
 "use client";
-import Image from "next/image";
 import { useState, useRef, useEffect } from "react";
-import {
-  FaPlayCircle,
-  FaPauseCircle,
-  FaStepBackward,
-  FaStepForward,
-  FaVolumeMute,
-  FaSearch,
-} from "react-icons/fa";
 import { CgSpinnerAlt } from "react-icons/cg";
 import { searchAlbums, getAlbumDetails } from "../../utils/api";
-import { useQuery, QueryClient } from "@tanstack/react-query";
-import RootLayout from "@/app/layout";
+import { useQuery } from "@tanstack/react-query";
 import AlbumLayout from "../layout";
+import Player from "@/app/components/player";
 
 interface pageProps {
   params: { albumName: string };
 }
 export default function Plyr({ params }: pageProps) {
-  const [isPlaying, setIsPlaying] = useState(false);
-  const audioEl = useRef<HTMLAudioElement>(null);
-  const progressBar = useRef<HTMLInputElement>(null);
-  let [currentTime, setCurrentTime] = useState(audioEl?.current?.currentTime);
   let [duration, setDuration] = useState();
   const [albumSongList, setAlbumSongList] = useState([]);
-  const [currentSong, setCurrentSong] = useState([]);
   const [albumSongImage, setAlbumSongImage] = useState();
   const [artistName, setArtistName] = useState();
   const [songUrl, setSongUrl] = useState();
   const [index, setIndex] = useState(0);
   const [label, setLabel] = useState();
-  const [songTime, setSongTime] = useState(0);
-  const [songImage, setSongImage] = useState();
   const [albumQuery, setAlbumQuery] = useState(params.albumName);
 
-  const [progress, setProgress] = useState(songTime);
-
-  useEffect(() => {
-    if (currentTime && duration) {
-      if (isNaN(currentTime)) {
-        setSongTime(0);
-      } else {
-        setSongTime((currentTime / duration) * 100);
-      }
-    }
-
-    const updateProgress = () => {
-      if (audioEl) {
-        const currentTime = audioEl?.current?.currentTime;
-        if (currentTime && duration) {
-          const newProgress = (currentTime / duration) * 100;
-
-          setProgress(newProgress);
-        }
-      }
-    };
-    if (isPlaying) {
-      audioEl?.current?.play();
-
-      const interval = setInterval(updateProgress, 100);
-
-      return () => {
-        clearInterval(interval);
-      };
-    } else {
-      audioEl?.current?.pause();
-    }
-  }, [isPlaying, progress, currentTime, duration]);
-
-  const onScrub = (value: number) => {
-    const audioElement = document.getElementById(
-      "audio"
-    ) as HTMLAudioElement | null;
-
-    if (audioElement && duration) {
-      audioElement.currentTime = (value * duration) / 100;
-    }
-    if (progress !== currentTime) {
-      if (!isPlaying) {
-        setIsPlaying(!isPlaying);
-      }
-    }
-  };
   const { data, isSuccess } = useQuery({
     queryKey: ["trackdetails"],
     queryFn: async () => {
@@ -121,105 +57,27 @@ export default function Plyr({ params }: pageProps) {
 
   return (
     <AlbumLayout>
-      <div className="p-10">
-        {/* <div className="flex justify-center"> */}
-        {/*   <input */}
-        {/*     className="text-black bg-gray-400 rounded-md w-full mb-4 h-10 p-5" */}
-        {/*     value={albumQuery} */}
-        {/*     onChange={(e) => { */}
-        {/*       setAlbumQuery(e.target.value); */}
-        {/*     }} */}
-        {/*   /> */}
-        {/*   <FaSearch className="h-10 w-10 text-white" /> */}
-        {/* </div> */}
-        <div className="font-poppins">
-          {data ? (
-            <>
-              <div className="flex justify-center">
-                {albumSongImage ? (
-                  <Image
-                    width={600}
-                    height={500}
-                    src={albumSongImage}
-                    alt="image"
-                    className="rounded-md"
-                  />
-                ) : (
-                  <Image
-                    width={600}
-                    height={500}
-                    src="someimage.jpg"
-                    alt="image"
-                    className="rounded-md"
-                  />
-                )}
-              </div>
-              <div className="justify-center ">
-                <audio
-                  id="audio"
-                  ref={audioEl}
-                  src={songUrl}
-                  onEnded={() => {
-                    skipToNext();
-                    setSongTime(0);
-                    setIsPlaying(true);
-                  }}
-                  onPause={() => setIsPlaying(false)}
-                  onPlay={() => setIsPlaying(true)}
-                ></audio>
-              </div>
-              <div>
-                <p className="text-white mt-2 text-2xl">{label}</p>
-                <p className="text-gray-400 text-sm">{artistName}</p>
-              </div>
-              <div className="flex justify-center mt-5">
-                <input
-                  type="range"
-                  value={progress}
-                  ref={progressBar}
-                  max={100}
-                  onChange={(e) => {
-                    e.preventDefault;
-                    onScrub(parseInt(e.target.value));
-                  }}
-                  className="w-full md:w-1/4 shadow-xl rounded-full"
-                />
-              </div>
-
-              <div className="text-white flex justify-between md:px-72">
-                <p>{audioEl.current?.currentTime}</p>
-                <p>{duration}</p>
-              </div>
-              <div className="flex justify-center p-10 gap-10">
-                <FaStepBackward
-                  className="text-white h-10 w-10 active:text-gray-400"
-                  onClick={skipBack}
-                />
-                {isPlaying ? (
-                  <FaPauseCircle
-                    className="text-white  h-12 w-12 active:text-gray-400"
-                    onClick={() => setIsPlaying(false)}
-                  />
-                ) : (
-                  <FaPlayCircle
-                    className="text-white  h-12 w-12 active:text-gray-400"
-                    onClick={() => setIsPlaying(true)}
-                  />
-                )}
-                <FaStepForward
-                  className="text-white  h-10 w-10 active:text-gray-400"
-                  onClick={skipToNext}
-                />
-                {/* <FaVolumeMute className="text-white  h-10 w-10" /> */}
-              </div>
-            </>
-          ) : (
-            <div className="justify-center flex">
-              <CgSpinnerAlt className="animate-spin h-10 w-10 text-white" />
-            </div>
-          )}
+      {songUrl &&
+      duration &&
+      albumSongImage &&
+      albumSongImage &&
+      label &&
+      artistName ? (
+        <Player
+          songUrl={songUrl}
+          songDuration={duration}
+          songList={albumSongList}
+          songImage={albumSongImage}
+          songName={label}
+          songArtist={artistName}
+          skipToNext={skipToNext}
+          skipBack={skipBack}
+        />
+      ) : (
+        <div className="justify-center flex">
+          <CgSpinnerAlt className="animate-spin h-10 w-10 text-white" />
         </div>
-      </div>
+      )}
     </AlbumLayout>
   );
 }
