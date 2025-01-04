c2283b451a94fa46537f8031fbac205c87361098
Debiprasad Das <debiprasadxd@protonmail.com>
2025-01-04 12:26:06 +0530

    add new changes

---
+++ src/app/comps/player.tsx
@@ -0,0 +1,45 @@
+"use client";
+import AudioPlayer from "react-h5-audio-player";
+import "react-h5-audio-player/lib/styles.css";
+import Image from "next/image";
+interface Props {
+  audioUrl: string;
+  thumbUrl: string;
+  handlePlay: () => void;
+  handlePause: () => void;
+  handleNext: () => void;
+  handlePrevious: () => void;
+}
+
+const Player: React.FC<Props> = ({
+  audioUrl,
+  thumbUrl,
+  handlePlay,
+  handlePause,
+  handleNext,
+  handlePrevious,
+}) => {
+  return (
+    <div>
+      <div style={{ display: "flex", justifyContent: "center" }}>
+        <Image src={thumbUrl} width={400} height={400} alt="image"></Image>
+      </div>
+      <AudioPlayer
+        src={audioUrl}
+        onPlay={handlePlay}
+        onPause={handlePause}
+        onClickNext={handleNext}
+        onClickPrevious={handlePrevious}
+        autoPlay={false}
+        showSkipControls={true}
+        progressJumpSteps={{ backward: 5000, forward: 5000 }}
+        style={{
+          backgroundColor: "#333333",
+          color: "#ffffff",
+        }}
+        className="my-custom-audio-player"
+      />
+    </div>
+  );
+};
+export default Player;
